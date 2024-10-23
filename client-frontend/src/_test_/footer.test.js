fbea5406cf715a7cc7aa5f736b3d951f67e6b7d9
marie-adi <marie.adim@gmail.com>
2024-10-23 10:11:49 +0200

    test: clicking links

--- client-frontend/src/_test_/footer.test.js
+++ client-frontend/src/_test_/footer.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Footer from '../components/Footer.jsx';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Footer', () => {
     test('render the Footer', () => {
@@ -13,4 +15,20 @@ describe('Footer', () => {
       expect(screen.getByText('Síguenos')).toBeInTheDocument();
       expect(screen.getByText(/© 2024 Bio Blog/i)).toBeInTheDocument(); /* esto es para saber si cambia */
     });
+  
+
+  test('navigates to correct page when clicking links', () => {
+    render(
+      <MemoryRouter>
+        <Footer />
+      </MemoryRouter>
+    );
+
+    /* Simula clic en un enlace */
+    const aboutLink = screen.getByText('Acerca de nosotros');
+    userEvent.click(aboutLink);
+
+   /*  Verifica si después del clic cambia la URL */
+    expect(window.location.pathname).toBe('/about');    /* Cambia '/about' por la ruta correcta esperada */
   });
+});
