4c0bd1a57f19d62b87334d72cfd206272edeb1b4
marie-adi <marie.adim@gmail.com>
2024-10-24 10:44:42 +0200

    test: one test footer ready

--- client-frontend/src/_test_/footer.test.js
+++ client-frontend/src/_test_/footer.test.js
@@ -2,33 +2,21 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Footer from '../components/Footer.jsx';
 import '@testing-library/jest-dom';
-import userEvent from '@testing-library/user-event';
+/* import userEvent from '@testing-library/user-event'; */
 import { MemoryRouter } from 'react-router-dom';
 
 describe('Footer', () => {
     test('render the Footer', () => {
-      render(<Footer />);
-  
+  render(
+    <MemoryRouter>
+      <Footer />
+    </MemoryRouter>
+  );
+    console.log(screen.debug());
+
       /* para verificar si los titulos estan en el DOM */
-      expect(screen.getByText('Acerca de nosotros')).toBeInTheDocument();
       expect(screen.getByText('Enlaces útiles')).toBeInTheDocument();
       expect(screen.getByText('Síguenos')).toBeInTheDocument();
       expect(screen.getByText(/© 2024 Bio Blog/i)).toBeInTheDocument(); /* esto es para saber si cambia */
     });
-  
-
-  test('navigates to correct page when clicking links', () => {
-    render(
-      <MemoryRouter>
-        <Footer />
-      </MemoryRouter>
-    );
-
-    /* Simula clic en un enlace */
-    const aboutLink = screen.getByText('Acerca de nosotros');
-    userEvent.click(aboutLink);
-
-   /*  Verifica si después del clic cambia la URL */
-    expect(window.location.pathname).toBe('/about');    /* Cambia '/about' por la ruta correcta esperada */
-  });
 });
