61a22ed029e01097748e01d267d5742bdb36a93a
marie-adi <marie.adim@gmail.com>
2024-10-24 11:03:06 +0200

    test: second test ready

--- client-frontend/src/_test_/footer.test.js
+++ client-frontend/src/_test_/footer.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Footer from '../components/Footer.jsx';
 import '@testing-library/jest-dom';
-/* import userEvent from '@testing-library/user-event'; */
+import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('Footer', () => {
@@ -18,5 +18,19 @@ describe('Footer', () => {
       expect(screen.getByText('Enlaces útiles')).toBeInTheDocument();
       expect(screen.getByText('Síguenos')).toBeInTheDocument();
       expect(screen.getByText(/© 2024 Bio Blog/i)).toBeInTheDocument(); /* esto es para saber si cambia */
+    });
+
+    test('check if the links in the Footer work', async () => {
+      render(
+        <MemoryRouter>
+          <Footer />
+        </MemoryRouter>
+      );
+  
+      const user = userEvent.setup(); /* inicia UserEvent */
+
+      const githubLink = screen.getByText('GitHub').closest('a'); /* simulo el click en el enlace siguenos y encuentra el enlace mas cercano */
+      expect(githubLink).toHaveAttribute('href', 'https://github.com/Omarlsant/bio-blog/tree/dev'); /* verifico si el enlace existe y si cambia a la ruta correcta */
+      await user.click(githubLink);/* simulo el click en el enlace */
     });
 });
