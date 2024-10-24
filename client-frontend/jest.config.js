20f461a8eb1c3c609b045815c90d49a55355b633
marie-adi <marie.adim@gmail.com>
2024-10-24 11:36:20 +0200

    test: init test navBar, one test ready

--- client-frontend/jest.config.js
+++ client-frontend/jest.config.js
@@ -2,4 +2,7 @@
     testEnvironment: 'jsdom',
     testEnvironment: 'jest-environment-jsdom',
     verbose: false,
+    moduleNameMapper: {
+      '\\.(jpg|jpeg|png|gif|webp|svg|mp4)$': '<rootDir>/__mocks__/fileMock.js', /* Maneja importaciones de archivos estáticos como imágenes y videos */
+    },
   };
