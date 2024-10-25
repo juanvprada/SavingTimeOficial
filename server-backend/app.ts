1b4b36cea9b9a95e36d53ce324879905fe7d21d9
Omar Lengua <omarns21@gmail.com>
2024-10-25 11:26:31 +0200

    Implementation: Setting conection to db: done, setting routes in the back: done

--- server-backend/app.ts
+++ server-backend/app.ts
@@ -1,36 +1,43 @@
-import conectionDB from './database/conectionDB'; // Import database connection
-import catMeme from './models/catModel';  // Import model from table Meme
+import conectionDB from './database/conectionDb'; // Import database connection
+import PostBlog from './model/blogModel';  // Import model from table Meme
 import express from 'express';
 import cors from 'cors';
-import { router } from './routers/catRouter';
+import { router } from './router/blogRouter';
 import {PORT} from './config';
+import UserModel from './model/userModel';
 
-export const app = express();
+const app = express();
 
 app.use(cors({
   origin: 'http://localhost:5173', // el localhost donde esta corriendo el front
 }));
 app.use(express.json());
 
-app.use('/api/memes', router);
+app.use('/api/posts', router);
 
-  try {
-    await conectionDB.authenticate();  // Autheticas the database
-    console.log('Conexión a la base de datos exitosa');
-
-    // Fetch all memes
-   await catMeme.sync({ force: false });
-   console.log('Tabla de memes creada');
-
-   await userModel.sync({ force: false });
-   console.log('Tabla de usuarios creada');
-
-  } catch (error) {
-    console.error('Error al conectar o consultar la base de datos 😒:', error);
-  }
+  const initializeDatabase = async () => {
+    try {
+      await conectionDB.authenticate();  // Autheticas the database
+      console.log('Conexión a la base de datos exitosa');
+  
+      // Fetch all memes
+      await PostBlog.sync({ force: true });
+      console.log('Tabla de memes creada');
+  
+      await UserModel.sync({ force: false });
+      console.log('Tabla de usuarios creada');
+  
+    } catch (error) {
+      console.error('Error al conectar o consultar la base de datos 😒:', error);
+    }
+  };
+  
+  initializeDatabase();
 
 
 export const server = app.listen((PORT || 3000), () => {
   console.log(`Servidor corriendo en el puerto http://localhost:${PORT}/`);;
 });
+
+export default app;
 
