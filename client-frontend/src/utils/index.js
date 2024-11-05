76cc0c178b1796354f7009a3889f36a7a6525738
dario3111 <dario3pacheco@gmail.com>
2024-11-05 13:45:01 +0100

    Update branch

--- client-frontend/src/utils/index.js
+++ client-frontend/src/utils/index.js
@@ -1,10 +1,10 @@
 //Importar archivos
-import fotoBosque from '../assets/home-img/bosque.jpeg';
-import fotoRecicla from '../assets/home-img/recicla.webp'
-import fotoSolar from '../assets/home-img/solar.webp';
-import vidHome from '../assets/home-vid/video-home.mp4';
-import logoNav from '../assets/nav-bar/logo.png';
-import fotoConstruccion from '../assets/construccion.png'
+import fotoBosque from "../assets/home-img/bosque.jpeg";
+import fotoRecicla from "../assets/home-img/recicla.webp";
+import fotoSolar from "../assets/home-img/solar.webp";
+import vidHome from "../assets/home-video/video-home.mp4";
+import logoNav from "../assets/nav-bar/logo.png";
+import fotoConstruccion from "../assets/construccion.png";
 
 //Exportar los archivos importados
 export const bosqueImg = fotoBosque;
@@ -16,20 +16,20 @@ export const construccionImg = fotoConstruccion;
 
 //Definir los links de enlaces
 export const navLinks = [
-    {
-      id: "inicio",
-      title: "Inicio",
-    },
-    {
-      id: "blog",
-      title: "Blog",
-    },
-    {
-      id: "nosotros",
-      title: "Nosotros",
-    },
-    {
-      id: "contacto",
-      title: "Contacto",
-    },
-  ];
+  {
+    id: "inicio",
+    title: "Inicio",
+  },
+  {
+    id: "blog",
+    title: "Blog",
+  },
+  {
+    id: "nosotros",
+    title: "Nosotros",
+  },
+  {
+    id: "contacto",
+    title: "Contacto",
+  },
+];
